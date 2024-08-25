acf22fdddf061cafefe6bc52f00f38572e087fc9
aliyun <Jerrylzj0808@outlook.com>
2024-08-25 23:58:54 +0800

    ``` refactor(parallel-map): implement usage of Arc and Mutex for synchronization
    Replace the previous implementation of parallel_map that used unsynchronized
    data structures with a new implementation using Arc and Mutex for thread-safe
    access. This ensures correct concurrent access to the output vector without relying
    on implicit synchronization provided by the receiver's lifecycle.
    
    Additionally, the U type parameter bound has been updated to include Debug and Clone
    traits to accommodate Arc::try_unwrap and initialization of the output vector,
    respectively. The implementation now also utilizes crossbeam_channel for efficient
    communication between threads.
    
    The commit ensures that the output vector can be safely accessed and modified by
    multiple threads concurrently, preventing potential race conditions and ensuring
    the integrity of the computed results.
    ```

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -1,15 +1,44 @@
 use crossbeam_channel;
 use std::{thread, time};
-
+use std::sync::{Arc, Mutex};
+use std::fmt::Debug;
 fn parallel_map<T, U, F>(mut input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
 where
     F: FnOnce(T) -> U + Send + Copy + 'static,
     T: Send + 'static,
-    U: Send + 'static + Default,
+    U: Send + 'static + Default + Debug + Clone, // Add Debug trait for Arc::try_unwrap. Add Clone trait for initialization of output_vec.
 {
-    let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
+    let output_vec = Arc::new(Mutex::new(vec![U::default(); input_vec.len()])); 
     // TODO: implement parallel map!
-    output_vec
+    let (sender, receiver) = crossbeam_channel::unbounded();
+    let mut threads = Vec::new();
+    for _ in 0..num_threads {
+        let receiver = receiver.clone();
+        let output_vec = Arc::clone(&output_vec);
+        threads.push(thread::spawn(move || {
+            while let Ok((index, item)) = receiver.recv() {
+                let index_num = (index, f(item));
+                output_vec.lock().unwrap()[index_num.0] = index_num.1;
+            }
+        }))
+    }
+
+    while let Some(item) = input_vec.pop() {
+        let index = input_vec.len();
+        sender.send((index, item)).expect("Failed to send item");
+    };
+
+    drop(sender);
+
+    for thread in threads {
+        thread.join().expect("Thread failed to complete");
+    }
+    
+    Arc::try_unwrap(output_vec)
+        .expect("Failed to unwrap Arc")
+        .into_inner()
+        .expect("Failed to unlock Mutex")
+
 }
 
 fn main() {
